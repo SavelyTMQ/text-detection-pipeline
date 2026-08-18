88ac324e8160c4dcb882a31cf56991bc58cc7a67
SavelyTMQ <82259064+SavelyTMQ@users.noreply.github.com>
2026-08-18 10:44:20 +0300

    Create __init__.py

---
+++ segmentation/models/__init__.py
@@ -0,0 +1,5 @@
+from segmentation.models.segnet import SegNet_Tiny
+from segmentation.models.unet import UNet
+from segmentation.models.deeplabv3 import get_deeplabv3
+
+__all__ = ["SegNet_Tiny", "UNet", "get_deeplabv3"]
